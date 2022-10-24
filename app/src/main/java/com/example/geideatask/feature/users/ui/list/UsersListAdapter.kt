5d6b68a7eb66f97e3688ba8cf0fbfaf0cc06b0ce
gohary <gohary@tempo.fit>
2022-10-25 00:11:11 +0200

    Setup users adapter

---
+++ app/src/main/java/com/example/geideatask/feature/users/ui/list/UsersListAdapter.kt
@@ -0,0 +1,56 @@
+package com.example.geideatask.feature.users.ui.list
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.geideatask.R
+import com.example.geideatask.databinding.ItemUserBinding
+import com.example.geideatask.feature.users.data.database.User
+
+class UsersListAdapter : ListAdapter<User, UsersListAdapter.DevicesViewHolder>(DiffUtilCallback()) {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int,
+    ): DevicesViewHolder {
+        return DevicesViewHolder(
+            ItemUserBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: DevicesViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class DevicesViewHolder(
+        private val binding: ItemUserBinding,
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(user: User) = with(binding) {
+            tvUserId.text = user.id.toString()
+            tvUserId.text = binding.root.context.getString(
+                R.string.user_name_place_holder,
+                user.first_name,
+                user.last_name
+            )
+
+        }
+    }
+}
+
+private class DiffUtilCallback : DiffUtil.ItemCallback<User>() {
+
+    override fun areItemsTheSame(
+        oldItem: User,
+        newItem: User,
+    ) = oldItem.id == newItem.id
+
+    override fun areContentsTheSame(
+        oldItem: User,
+        newItem: User,
+    ) = oldItem == newItem
+}
