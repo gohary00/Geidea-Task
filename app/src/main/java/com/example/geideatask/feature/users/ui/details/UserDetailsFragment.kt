1b1b049aaf55c814832ad64327822f0d67213752
gohary <gohary@tempo.fit>
2022-10-26 00:45:22 +0200

    Service setup

--- app/src/main/java/com/example/geideatask/feature/users/ui/details/UserDetailsFragment.kt
+++ app/src/main/java/com/example/geideatask/feature/users/ui/details/UserDetailsFragment.kt
@@ -1,18 +1,26 @@
 package com.example.geideatask.feature.users.ui.details
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.content.ContextCompat.RECEIVER_EXPORTED
+import androidx.core.content.ContextCompat.registerReceiver
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.example.geideatask.R
 import com.example.geideatask.databinding.FragmentUserDetailsBinding
 import com.example.geideatask.feature.users.data.database.User
 import com.example.geideatask.feature.users.data.models.shared.State
+import com.example.geideatask.feature.users.ui.service.CountDownService
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -23,6 +31,9 @@ class UserDetailsFragment : Fragment() {
 
     private val userDetailsViewModel: UserDetailsViewModel by viewModels()
     private val args: UserDetailsFragmentArgs by navArgs()
+
+    private lateinit var serviceIntent: Intent
+    private var time = 6.0
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,6 +48,18 @@ class UserDetailsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         userDetailsViewModel.getUserDetails(args.userId)
         setupObservers()
+        setupTimerService()
+    }
+
+    private fun setupTimerService() {
+        serviceIntent = Intent(requireContext(), CountDownService::class.java)
+        registerReceiver(
+            requireContext(),
+            updateTime,
+            IntentFilter(CountDownService.TIMER_UPDATED),
+            RECEIVER_EXPORTED
+        )
+        startTimer()
     }
 
     private fun setupObservers() {
@@ -67,8 +90,23 @@ class UserDetailsFragment : Fragment() {
         }
     }
 
+    private val updateTime: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            time = intent.getDoubleExtra(CountDownService.TIME_EXTRA, 0.0)
+            binding.tvTimer.text = time.toString()
+            if (time == 0.0)
+                findNavController().popBackStack()
+        }
+    }
+
+    private fun startTimer() {
+        serviceIntent.putExtra(CountDownService.TIME_EXTRA, time)
+        requireActivity().startService(serviceIntent)
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
+        requireActivity().stopService(serviceIntent)
         _binding = null
     }
 }
